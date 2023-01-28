63c679acf13e4bd9846629f35d1916405ee24688
shinymcshine09 <js.hart@hotmail.com>
2023-01-28 15:59:23 +0000

    got rid of jameschat for test

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import JamesChat from './components/JamesChat';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App /><JamesChat />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
